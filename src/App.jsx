f8975aed518809f1a59610be1a795ab09099647d
Balogun2705 <balogunsemiat2019@gmail.com>
2024-11-29 14:01:29 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import './app.css';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { AuthProvider } from './context/AuthContext';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
-    <AuthProvider>
-    <Navbar />
-      <main className='min-h-screen max-w-screen-2xl mx-auto font-primary'>
-      <Outlet /> { /*Outlet is where the children will be displayed */}
-      </main>
-      <Footer />
-    </AuthProvider>
+      <AuthProvider>
+        <Navbar />
+        <main className="min-h-screen max-w-screen-2xl mx-auto font-primary">
+          <Outlet /> {/*Outlet is where the children will be displayed */}
+        </main>
+        <Footer />
+      </AuthProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
